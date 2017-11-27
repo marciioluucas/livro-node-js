37476f45514cf2f54c42f272950d1be98940a531
Marcio Lucas <marciioluucas@gmail.com>
2017-11-27 21:40:42 -0200

    Pagina 61

--- ntalk/app.js
+++ ntalk/app.js
@@ -1,11 +1,18 @@
-var express = require('express');
-var load = require('express-load');
+let express = require('express');
+let load = require('express-load'),
+bodyParser = require('body-parser'),
+cookieParser = require('cookie-parser'),
+expressSession = require('express-session');
 
 
-var app = express();
+let app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(cookieParser('ntalk'));
+app.use(expressSession());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(__dirname + '/public'));
 
 load('models')
@@ -14,7 +21,6 @@ load('models')
     .into(app);
 
 app.listen(3000, function () {
-
     console.log("Servidor nas alturas! porta 3000");
 });
 
